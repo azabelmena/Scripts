8b7d8d6bf394c330b429c891600affa70ad6898e
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2023-12-01 01:35:26 -0400

    Update nixcp.c

--- nixcp.c
+++ nixcp.c
@@ -10,19 +10,13 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
+    const char *machine = argv[1];
 
-    if(strstr("cayley", argv[1])){
-        system("sudo cp /home/alec/.config/nixos/cayley/* /etc/nixos/");
-        /*system("sudo cp /home/alec/.config/nixos/cayley/configuration.nix /etc/nixos/configuration.nix");*/
-        /*system("sudo cp /home/alec/.config/nixos/cayley/nvidia.nix /etc/nixos/nvidia.nix");*/
-        /*system("sudo cp /home/alec/.config/nixos/cayley/packages.nix /etc/nixos/packages.nix");*/
+    if(strstr("cayley", machine)){
+        system("sudo cp /home/alec/.config/nix/cayley/* /etc/nix/");
     }
-    else if(strstr("cauchy", argv[1])) {
-        system("sudo cp /home/alec/.config/nixos/cauchy/* /etc/nixos/");
-
-        /*system("sudo cp /home/alec/.config/nixos/cauchy.g/configuration.nix /etc/nixos/configuration.nix");*/
-        /*system("sudo cp /home/alec/.config/nixos/cauchy.g/nvidia.nix /etc/nixos/nvidia.nix");*/
-        /*system("sudo cp /home/alec/.config/nixos/cauchy.g/packages.nix /etc/nixos/packages.nix");*/
+    else if(strstr("cauchy", machine)) {
+        system("sudo cp /home/alec/.config/nix/cauchy/* /etc/nix/");
     }
     else{
         fprintf(stderr, "Invalid arguments.\n");
